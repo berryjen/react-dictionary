ba1e5a3637a38fd54448501b86d7db35c2b73323
Jen <73888356+berryjen@users.noreply.github.com>
2021-03-29 12:24:28 +0100

    integrated api call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./montenegro.png";
 import './App.css';
+import Footer from "./Footer";
+import "./style.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
+        
       </header>
+        <main>
+          <Dictionary />
+        </main>
+      <Footer />
+      </div>
     </div>
   );
 }
