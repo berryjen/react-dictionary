a5e257e286f7f24d5ba1f0838b8a14e0bd7e8d6b
Jen <73888356+berryjen@users.noreply.github.com>
2021-03-30 16:53:58 +0100

    uppercased phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ import ReactAudioPlayer from "react-audio-player";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-    <ReactAudioPlayer src={phonetic.audio} controls />
+    <ReactAudioPlayer src={Phonetic.audio} controls />
      {props.phonetic.text}
     </div>
   );
