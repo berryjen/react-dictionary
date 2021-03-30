f633fae529a36c62cc7c2a3d168b6bc43596e18b
Jen <73888356+berryjen@users.noreply.github.com>
2021-03-30 22:24:23 +0100

    corrected phonetic spelling

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ import ReactAudioPlayer from "react-audio-player";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-    <ReactAudioPlayer src={Phonetic.audio} controls />
+    <ReactAudioPlayer src={props.phonetic.audio} controls />
      {props.phonetic.text}
     </div>
   );
--- src/Results.js
+++ src/Results.js
@@ -7,8 +7,8 @@ export default function Results(props) {
     return (
     <div className="Results">
       <h2>{props.results.word}</h2>
-      <Phonetic props={props.results.phonetic} />
-      {props.results.phonetic.map(function(phonetic, index) {
+     
+      {props.results.phonetics.map(function(phonetic, index) {
          return (
            <div key={index}>
              <Phonetic phonetic={phonetic} />
